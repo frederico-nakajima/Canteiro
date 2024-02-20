798f9cec2c4ca4bf638cc0b77132b75330ee8368
frederico nakajima <fredericonakajimanakajima@gmail.com>
2024-02-20 03:15:05 -0300

    implementação de darkMode

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggle(){
+    const html = document.documentElement
+    html.classList.toggle("darkMode")
+
+    
+    
+
+
+}
+
+
+// const myhtml = document.getElementById('myhtml')
+  
+
+//     myhtml.classList.toggle('darkMode')
